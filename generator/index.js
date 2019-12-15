f744499f2e56ff0ad2d4104a42a7eb555201602c
hpstream <915414300@qq.com>
2019-12-15 13:45:01 +0800

    feat:初始化工程

--- generator/index.js
+++ generator/index.js
@@ -1,7 +1,38 @@
 module.exports = (api, options, rootOptions) => {
   
-
+   api.extendPackage({
+     scripts: {
+       test: 'vue-cli-service test'
+     },
+     "husky": {
+       "hooks": {
+         "pre-commit": "lint-staged"
+       }
+     },
+     "lint-staged": {
+       "src/**/*.js": [
+         "eslint --fix",
+         "git add"
+       ],
+       "src/**/*.less": [
+         "stylelint --fix",
+         "git add"
+       ]
+     },
+     "devDependencies": {
+       "eslint": "^5.16.0",
+       "husky": "^2.3.0",
+       "lint-staged": "^7.2.2",
+       "stylelint": "^10.0.1"
+     },
+     "dependencies": {
+       "front-common": "git+ssh://git@git.wb-intra.com:web_group/front-common.git",
+     }
+   })
   // 复制并用 ejs 渲染 `./template` 内所有的文件
   api.render('./template')
+  console.log('------------------------------------');
+  console.log(11333);
+  console.log('------------------------------------');
 
 }
--- generator/template/README.md
+++ generator/template/README.md
@@ -1,16 +1,3 @@
-
-rem.js:
-https://qiniuh5.wodidashi.com/web/front-static/common/rem/1.0/rem.js
-https://qiniuh5.wodidashi.com/web/front-static/common/rem/1.0/rem.min.js
-
-
-vconsole:
-https://qiniuh5.wodidashi.com/web/front-static/common/vconsole/v3.3.0/vconsole.min.js
-
-去除默认样式css
-
-https://qiniuh5.wodidashi.com/web/front-static/common/wb/1.0/base.css
-
-
+# demo
 
 
